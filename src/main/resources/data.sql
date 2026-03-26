84622498d6f30ca980d09e0782c9b0cd8c07aac2
TIRUVALLURYAKHILA <atiruvallury@gmail.com>
2026-03-26 14:46:22 +0530

    docker setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, password, role, status)
+VALUES ('Admin', 'admin@gmail.com', '123456', 'admin', 'active');
+
+INSERT INTO users (name, email, password, role, status)
+VALUES ('User', 'user@gmail.com', '123456', 'user', 'active');
